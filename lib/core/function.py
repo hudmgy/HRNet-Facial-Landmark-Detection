7405b2cf6956312febfeb2a011c3bebf9df0e848
Tyger Chai <hudmgy@163.com>
2019-08-29 14:59:52 +0800

    preds, conf = decode_preds(score_map, meta[center], meta[scale], [64, 64])

--- lib/core/function.py
+++ lib/core/function.py
@@ -66,7 +66,7 @@ def train(config, train_loader, model, critertion, optimizer,
 
         # NME
         score_map = output.data.cpu()
-        preds = decode_preds(score_map, meta['center'], meta['scale'], [64, 64])
+        preds, conf = decode_preds(score_map, meta['center'], meta['scale'], [64, 64])
 
         nme_batch = compute_nme(preds, meta)
         nme_batch_sum = nme_batch_sum + np.sum(nme_batch)
@@ -131,7 +131,7 @@ def validate(config, val_loader, model, criterion, epoch, writer_dict):
             # loss
             loss = criterion(output, target)
 
-            preds = decode_preds(score_map, meta['center'], meta['scale'], [64, 64])
+            preds, conf = decode_preds(score_map, meta['center'], meta['scale'], [64, 64])
             # NME
             nme_temp = compute_nme(preds, meta)
             # Failure Rate under different threshold
